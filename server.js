ecc6c14fa189998ebff268435d9798106db15feb
Faizanur Rahman <faizanurahman1711@gmail.com>
2025-05-18 20:54:29 +0530

    route fixed

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-app.get('/api/number', (req,res) => {
-    fs.readFile(path.join(__dirname, 'storage.json'), 'utf8' , (err, data) => {
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/api/number', (req, res) => {
+    fs.readFile(path.join(__dirname, 'storage.json'), 'utf8', (err, data) => {
         if (err) {
             return res.status(500).json({ error: 'Internal Server Error' });
         }
